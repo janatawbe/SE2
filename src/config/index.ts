d3dba7864d8955493eb95c61624b441bd86b1214
janatawbe <jana.abbas.tawbe@gmail.com>
2025-09-13 13:14:49 +0300

    Added logging module

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ import path from 'path';
 dotenv.config({path: path.join(__dirname, '../../.env')});
 
 export default {
-    secret: process.env.secret || 'default secret'
+    NODE_ENV: process.env.NODE_ENV || 'development', // Checks if the environment type (e.g., 'production' or 'development') is set; if not, it uses 'development' as default.
+    logDir: 'logs', // Specifies the folder where log files will be saved.
 }
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ import config from "./config";
 
 const name = 'jana';
 console.log(`Hello, ${name}!`);
-console.log("Secret is:", config.secret);
